0faf98960f7272f427f2a5d52c604600b6348501
Ken <26967723+CodeRacoon@users.noreply.github.com>
2020-02-19 14:30:26 +0100

    Updates

---
+++ precache-manifest.6ea03d6067abe22e7056fa517902a4b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8941ef932e73117ae00b8097685ed356",
+    "url": "/F1-Calender/index.html"
+  },
+  {
+    "revision": "b6a72e6d2f5d1491806e",
+    "url": "/F1-Calender/static/css/2.cd67af34.chunk.css"
+  },
+  {
+    "revision": "8b2e534abc9ffab9df9a",
+    "url": "/F1-Calender/static/css/main.db281c63.chunk.css"
+  },
+  {
+    "revision": "b6a72e6d2f5d1491806e",
+    "url": "/F1-Calender/static/js/2.7263c73e.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/F1-Calender/static/js/2.7263c73e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b2e534abc9ffab9df9a",
+    "url": "/F1-Calender/static/js/main.b75b602d.chunk.js"
+  },
+  {
+    "revision": "55d2d149174dca849f06",
+    "url": "/F1-Calender/static/js/runtime-main.a3a19146.js"
+  },
+  {
+    "revision": "23e2b6cf75076c68a788053631d1f14d",
+    "url": "/F1-Calender/static/media/ep_naturalblack.23e2b6cf.png"
+  }
+]);
